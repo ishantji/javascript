995626deb3a445388afab72a728e92426f0a0e76
Ishant <57654359+ishantji@users.noreply.github.com>
2025-07-01 12:07:26 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "hello";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// "hello" => NaN 
+// true => 1 ; false => 0
+
+let isLoogedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoogedIn);
+//console.log(booleanIsLoggedIn);
+
+// 1 =>  true; 0 => false
+// "" => false
+// "ishant" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
