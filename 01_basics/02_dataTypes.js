468d0372fb6eb4d81ae6f580d5fdaec05e50de9f
Ishant <57654359+ishantji@users.noreply.github.com>
2025-07-01 11:11:05 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+console.log( 3 
+    + 3); // code readability should be high
+
+console.log("hello")
+
+
+let name = "Ishant"
+let age = 24
+let isLoggedIn = false
+
+// primitive data types
+
+// number => 2 to pwer 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// non primitive data type
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
