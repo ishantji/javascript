2f29efe44e0ca3f00fb4060d333f0de2781369ae
Ishant <57654359+ishantji@users.noreply.github.com>
2025-07-10 03:41:43 +0000

    maths & nums

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,48 @@
+//game score
+const score = 300;
+//console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+//console.log(typeof balance);
+
+const otherNumber = 123.8966
+
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ++++++++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-6));
+// console.log(Math.round(100.4));
+// console.log(Math.floor(100.4));
+// console.log(Math.ceil(100.8));
+// console.log(Math.min(6, 8, 9, 1));
+// console.log(Math.max(6, 8, 9, 1));
+
+// random value in between 0,1
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min);
+
+
+
+
+
+
+
+
+
