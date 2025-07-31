68250468021313e4d36d9e435cfa3c6da2ac6050
Ishant <57654359+ishantji@users.noreply.github.com>
2025-07-31 03:34:07 +0000

    scope level & mini hoisting in js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,12 +6,55 @@ if (true) {
     let a = 10;
     const b = 20;
     //c = 30;
-    console.log(`Inner: ${a}`);
+    //console.log(`Inner: ${a}`);
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
 
 //scope inside browser console is different from scope inside termial
+//global can be access anywhere inside scope also
+//but local can not access out side the scope
+
+function one(){
+    const username = "Ishant";
+
+    function two(){
+        const website = "youtube";
+        console.log(username);
+    }
+
+    //console.log(website);
+    two();    
+}
+
+//one();
+
+if (true) {
+    const username = "Ishu";
+    if(username === "Ishu") {
+        const website = " youtube";
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+
+// +++++++++++++++ interesting +++++++++++++++++++
+
+//works properly because it is function
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+//hoisting
+//can not access because it is used in const
+console.log(addtwo(5));
+const addtwo = function(num){
+    return num + 2
+}
