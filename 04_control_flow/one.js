7c97236bd75eb026ebc47611993aca0e41e7a847
Ishant <57654359+ishantji@users.noreply.github.com>
2025-08-01 05:14:02 +0000

    control flow in js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,33 @@
+// if
+const isUserLoggedIn = true;
+const temperature = 39
+// if ( temperature <= 50){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+// console.log("Execute");
+
+
+// <    3<5
+// >    6>2
+// <=   2<=2
+// >=   7>=5
+// ==   2==2
+// !=   2!=1
+// ===  2=='2'
+// !==  2!=='2'
+
+// const score = 200
+
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);    
+// }
+
+// console.log(`User Power: ${power}`);
+
+
+const balance = 1000
+
+if (balance > 500) console.log("Yes above 500"),console.log("test 2");
