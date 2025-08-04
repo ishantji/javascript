1c6b2bdbbadfe0eaa9df39a382178a3e02f05576
Ishant <57654359+ishantji@users.noreply.github.com>
2025-08-04 17:24:06 +0000

    while & do while in js

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+//print 1 to 10 using while loop
+let index = 1
+// while (index <= 10) {
+//     console.log(index);
+//     index++;
+// }
+
+let myArray = ["red", "blue", "orange"];
+let j = 0;
+// while (j < myArray.length) {
+//     console.log(`Value is ${myArray[j]}`);
+//     j++;
+// }
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
