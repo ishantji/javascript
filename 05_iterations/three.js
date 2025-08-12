6a9a3e269938c382d496bea79a175872cd8daacb
Ishant <57654359+ishantji@users.noreply.github.com>
2025-08-12 17:13:29 +0000

    for of in js

---
+++ 05_iterations/three.js
@@ -0,0 +1,46 @@
+// for of
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+const arr = [1, 2, 3, 4, 5]
+
+//array values
+for (const num of arr) {
+    //console.log(num);
+}
+
+const greetings = "Hello World!";
+for (const greet of greetings) {
+    //console.log(`Each Value is: ${greet}`);
+}
+
+//array keys
+for (const num in arr) {
+    //console.log(num);
+}
+
+//maps
+const map = new Map();
+map.set('IN', "India")
+map.set('FR', "France")
+map.set('FR', "France")
+map.set('USA', "United States of America")
+map.set('FR', "France")
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key,':-', value);
+}
+
+const myObject = {
+    "game1": "NFS",
+    "game2": "Spiderman"
+}
+console.log(myObject);
+
+// for (const [key,value] of myObject) {
+//     //console.log(key, ':-', value);
+// }
+
