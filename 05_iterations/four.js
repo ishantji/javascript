13ab41673d5898fed03b4f744a1802a90cc3fda3
Ishant <ishantittechnology@gmail.com>
2025-08-13 00:02:24 +0530

    add learn object

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const language = {
+    'js' : 'Javascript',
+    'cpp' : 'c++'
+}
+
+//console.log(language);
+for (const [key,value] of Object.entries(language)) {
+    //console.log(key, ':-', value);
+}
+
+for (const key of Object.keys(language)) {
+   // console.log(key);
+}
+
+for (const value of Object.values(language)) {
+    //console.log(value);
+}
+
+for (const key of Object.keys(language)) {
+    console.log(languagep[key]);
+}
